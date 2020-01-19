5684453a921fe6d9378617c277cabd12fe4fbbe3
Ivan Khuri <ivankhuri@gmail.com>
2020-01-19 13:02:37 +0200

    Step 5

--- server.js
+++ server.js
@@ -7,32 +7,18 @@ function handler(request, response) {
     var method = request.method;
     console.log(method);
 
-    switch (endpoint) {
-        case '/': {
-            response.writeHead(200, { "Content-Type": "text/html" });
-            fs.readFile(__dirname + '/public/index.html', (error, file) => {
-                if (error) {
-                    console.log('Error failed to load file index.html');
-                    return;
-                }
-                response.end(file);
-            });
-            break;
-        }
-        case '/node': {
-            response.writeHead(200, { 'Content-type': 'text/html' });
-            response.end('Node??');
-            break;
-        }
-        case '/girls': {
-            response.writeHead(200, { 'Content-type': 'text/html' });
-            response.end('Girls????');
-            break;
-        }
-        default: {
-            response.writeHead(404, { "Content-Type": "text/html" });
-            response.end('404 Content not found');
-        }
+    if (endpoint === '/') {
+
+        // ...
+    } else if (endpoint === '/node') {
+
+        //...
+    } else if (endpoint === '/girls') {
+
+        //...
+    } else {
+
+        // TODO - write your generic endpoint code here
     }
 }
 
