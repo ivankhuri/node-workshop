9bc98ad67093054b8bd15ce1557cf914c29b6ea7
Ivan Khuri <ivankhuri@gmail.com>
2020-01-19 12:21:23 +0200

    Step 3 commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const http = require('http');
-var server = http.createServer();
+
+function handler (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write('Response good veri good');
+    response.end();
+}
+
+var server = http.createServer(handler);
+
 server.listen(3000, function () {
 
     console.log("Listening on port 3000");
