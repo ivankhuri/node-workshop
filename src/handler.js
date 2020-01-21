cb2bced560f735a710fb6e1b6fd83e475e2d4da5
Ivan Khuri <ivankhuri@gmail.com>
2020-01-21 11:45:38 +0200

    Step 10

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const querystring = require('querystring');
+const posts = require('./posts.json')
 const extensionType = {
     html: { 'Content-Type': 'text/html' },
     css: { 'Content-Type': 'text/css' },
     js: { 'Content-Type': 'application/js' },
     ico: { 'Content-Type': 'image/x-icon' },
     png: { 'Content-Type': 'image/png' },
-    jpg: { 'Content-Type': 'image/jpg' }
+    jpg: { 'Content-Type': 'image/jpg' },
+    json: { 'Content-Type': 'application/json'}
 };
 
 var handler = function (request, response) {
     var url = (request.url === '/') ?
         path.join(__dirname, '..', 'public', 'index.html') :
         path.join(__dirname, '..', 'public', request.url);
+    if (request.url.includes('posts')) {
+        url = path.join(__dirname, request.url);
+        url += '.json';
+    }
     var extension = url.split('.')[1];
 
     console.log(url);
@@ -28,8 +34,16 @@ var handler = function (request, response) {
         request.on('end', function () {
             response.writeHead(302, { 'Location': '/' });
             var convertedData = querystring.parse(allTheData);
-            console.log(convertedData);
-            response.end();
+            console.log('Data: ', convertedData);
+            posts[Date.now()] = convertedData.post
+            fs.writeFile(path.join(__dirname, './posts.json'), JSON.stringify(posts), (err) => {
+                if (err) {
+                    console.log(err)
+                }
+
+
+                response.end();
+            })
         });
         return;
     }
@@ -47,89 +61,3 @@ var handler = function (request, response) {
 }
 
 module.exports = handler;
-/*const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring');
-
-var handler = function (request, response) {
-    var endpoint = request.url;
-
-    if (endpoint === '/') {
-        response.writeHead(200, { "Content-Type": "text/html" });
-
-        fs.readFile(path.join(__dirname, '../public/index.html'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint.includes('/main.css')) {
-        response.writeHead(200, { "Content-Type": "text/css" });
-
-        fs.readFile(path.join(__dirname, '../public/main.css'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint.includes('/script.js')) {
-        response.writeHead(200, { "Content-Type": "application/js" });
-
-        fs.readFile(path.join(__dirname, '../public/script.js'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint.includes('posts')) {
-        response.writeHead(200, { "Content-Type": "application/json" });
-
-        fs.readFile(path.join(__dirname, '../src/posts.json'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint.includes('logo2.png')) {
-        response.writeHead(200, { "Content-Type": "image/png" });
-
-        fs.readFile(path.join(__dirname, '../public/img/logo2.png'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint.includes('/image.jpg')) {
-        response.writeHead(200, { "Content-Type": "image/jpg" });
-
-        fs.readFile(path.join(__dirname, '../public/img/image.jpg'), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint === '/create-post') {
-        var allTheData = '';
-        request.on('data', function (chunkOfData) {
-            allTheData += chunkOfData;
-        });
-
-        request.on('end', function () {
-            response.writeHead(301, { 'Location': '/' });
-            var convertedData = querystring.parse(allTheData);
-            console.log(convertedData);
-            response.end();
-        });
-    } else {
-        response.writeHead(404, { "Content-Type": "text/html" });
-        response.end('404 Content not found');
-    }
-}
-
-module.exports = handler;*/
